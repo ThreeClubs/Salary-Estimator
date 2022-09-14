66168bc139f2ffdcb52e2bc24a8d98056d0cb311
Andrew Grenon <andrew.grenon@gmail.com>
2022-09-14 14:38:35 +1200

    Progress bar with % complete

--- salaryapp.js
+++ salaryapp.js
@@ -1,21 +1,26 @@
 const incomes = [
-  "$45,760 - $52,000 🤓 Consider upskilling at Mission Ready HQ",
-  "$52,001 - $62,400 🙂 You could be just starting out",
-  "$62,401 - $83,200 😎 Comfortably in the middle",
-  "$83,201 - $124,800 🤩 You're doing very well!",
-  "$124,800+ Wow! 🤯 Are you a senior developer?",
-];
+    "$45,760 - $52,000 🤓 Consider upskilling at Mission Ready HQ",
+    "$52,001 - $62,400 🙂 You could be just starting out",
+    "$62,401 - $83,200 😎 Comfortably in the middle",
+    "$83,201 - $124,800 🤩 You're doing very well!",
+    "$124,800+ Wow! 🤯 Are you a senior developer?",
+  ];
 
 const button = document.getElementById("button");
-let resultDisplay = document.getElementById("resultDisplay");
+
 
 function salaryEstimator() {
+
+  let resultDisplay = document.getElementById("resultDisplay");
+  let fancyBar = document.getElementById("fancyBar");
+  let fancyWait = document.getElementById("fancyWait");
+  let complete = document.getElementById("complete");
+  const fetchDemand = document.getElementsByName("demand");
+  const fetchSupply = document.getElementsByName("supply");
+ 
   function salaryModel() {
     let demand = 0;
     let supply = 0;
-
-    let fetchDemand = document.getElementsByName("demand");
-    let fetchSupply = document.getElementsByName("supply");
 
     for (i = 0; i < fetchDemand.length; i++) {
       if (fetchDemand[i].checked) {
@@ -52,11 +57,30 @@ function salaryEstimator() {
   const waitMessage = () =>
     (resultDisplay.textContent = "Calculating results...");
 
+  function progressBar() {
+    let a = 0;
+    let run = setInterval(frames, 50);
+    function frames() {
+      a++;
+      if(a == 101){
+        clearInterval(run);
+        fancyWait.style.display = "none";
+        complete.style.display = "block";
+      } else {
+        fancyWait.style.display = "block";
+        fancyBar.style.width = a +"%" 
+        complete.style.display = "none";
+        fancyBar.textContent = a +"%"
+      }
+    }
+  }
+
   const wait = (delay = 0) =>
     new Promise((resolve) => setTimeout(resolve, delay));
 
   waitMessage();
-  wait(3000).then(salaryModel);
+  progressBar();
+  wait(5000).then(salaryModel);
 }
 
 button.addEventListener("click", salaryEstimator);
